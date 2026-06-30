164f49bd30ef04e11ac85d734b6717e1bf3251ee
Stella Wong Kai Ning <swon0163@student.monash.edu>
2026-06-30 09:30:41 +0800

    Changes body from allergies to allallergies

--- kudox/app/order/page.tsx
+++ kudox/app/order/page.tsx
@@ -55,7 +55,7 @@ export default function EatPage() {
             const res = await fetch("/api/eat", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ cuisine, allergies }),
+                body: JSON.stringify({ cuisine, allergies: allAllergies }),
             });
 
             const data = await res.json();
